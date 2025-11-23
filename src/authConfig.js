540c3994e37b458f1473a51063113b67f09d6672
codewithajaydev <ajaybecse03@gmail.com>
2025-11-23 22:40:46 +0530

    pushh

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const authConfig = {
     clientId: 'oauth2-pkce-client',
     authorizationEndpoint: 'https://13.205.8.126:8443/realms/fitness-oauth2/protocol/openid-connect/auth',
     tokenEndpoint: 'https://13.205.8.126:8443/realms/fitness-oauth2/protocol/openid-connect/token',
-    redirectUri: 'http://localhost:5173',
+    redirectUri: 'https://fitness-tracker-frontend-gilt.vercel.app/',
     scope: 'openid profile email offline_access',
     onRefreshTokenExpire: (event) => event.logIn(),
   }
